ce24166f5135c730a67f2b1e82a299665eb0892d
Gabriel <victorgabriel1730@gmail.com>
2024-05-21 08:31:04 -0300

    fix: add a cors lib

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { routes } from './routes'
-import { deleteFiles } from './utils/deleteTmpFiles'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 app.use(routes)
 
